aea0c39b6a94219caec64762e5126cca68dc1be1
Tony Baines <tony.baines.1@gmail.com>
2012-10-13 11:13:04 +0100

    The remaining table and indexes for the movies DB

--- postgreSQL/day3/1.create_movies.sql
+++ postgreSQL/day3/1.create_movies.sql
@@ -4,6 +4,7 @@ CREATE EXTENSION cube;
 DROP TABLE genres CASCADE;
 DROP TABLE movies CASCADE;
 DROP TABLE actors CASCADE;
+DROP TABLE movies_actors CASCADE;
 
 CREATE TABLE genres (
 	name text UNIQUE,
@@ -18,4 +19,13 @@ CREATE TABLE actors (
 	actor_id SERIAL PRIMARY KEY,
 	name text
 );
+CREATE TABLE movies_actors (
+	movie_id integer REFERENCES movies NOT NULL,
+	actor_id integer REFERENCES actors NOT NULL,
+	UNIQUE (movie_id, actor_id)
+);
+CREATE INDEX movies_actors_movie_id ON movies_actors (movie_id);
+CREATE INDEX movies_actors_actor_id ON movies_actors (actor_id);
+CREATE INDEX movies_genres_cube ON movies USING gist (genre);
+
 
